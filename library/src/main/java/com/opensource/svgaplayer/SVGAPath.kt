96b9fa808d07497f77f4349ce832c73c1f6afbf0
PonyCui <cuis@vip.qq.com>
2017-11-08 19:09:23 +0800

    improve: use StringTokenizer replace split.

--- library/src/main/java/com/opensource/svgaplayer/SVGAPath.kt
+++ library/src/main/java/com/opensource/svgaplayer/SVGAPath.kt
@@ -3,9 +3,12 @@ package com.opensource.svgaplayer
 import android.graphics.Path
 import java.util.*
 
-val VALID_METHODS: List<String> = listOf("M", "L", "H", "V", "C", "S", "Q", "R", "A", "Z", "m", "l", "h", "v", "c", "s", "q", "r", "a", "z")
+private val VALID_METHODS: Set<String> = setOf("M", "L", "H", "V", "C", "S", "Q", "R", "A", "Z", "m", "l", "h", "v", "c", "s", "q", "r", "a", "z")
+private val regex = "([a-zA-Z])".toRegex()
 
-class SVGAPath(private val strValue: String) {
+class SVGAPath(originValue: String) {
+
+    private val replacedValue: String = originValue.replace(regex, "|||$1 ").replace(",", " ")
 
     private var cachedPath: Path? = null
 
@@ -15,78 +18,76 @@ class SVGAPath(private val strValue: String) {
             return
         }
         val cachedPath = Path()
-        var currentMethod = ""
-        val args = ArrayList<SVGAPoint>()
-        var argLast: String? = null
-        val items = strValue.split("[,\\s+]".toRegex()).dropLastWhile(String::isEmpty).toTypedArray()
-        for (item in items) {
-            if (item.isEmpty()) {
-                continue
-            }
-            val firstLetter = item.substring(0, 1)
+        val segments = StringTokenizer(this.replacedValue, "|||")
+        while (segments.hasMoreTokens()) {
+            val segment = segments.nextToken()
+            if (segment.isEmpty()) { continue }
+            val firstLetter = segment.substring(0, 1)
             if (VALID_METHODS.contains(firstLetter)) {
-                argLast?.takeIf { it.isNotEmpty() }?.let {
-                    args.add(SVGAPoint(0.0f, 0.0f, try {it.toFloat()} catch (e: Exception) { 0.0f }))
-                }
-                this.operate(cachedPath, currentMethod, args)
-                args.clear()
-                currentMethod = firstLetter
-                argLast = item.substring(1)
-            } else {
-                argLast = if (null != argLast && argLast.trim { it <= ' ' }.isNotEmpty()) {
-                    args.add(SVGAPoint(try {
-                        argLast.toFloat()
-                    } catch (e: Exception) {0.0f}, try {
-                        item.toFloat()
-                    } catch (e: Exception) {0.0f}, 0.0f))
-                    null
-                } else {
-                    item
-                }
+                operate(cachedPath, firstLetter, StringTokenizer(segment.substring(1).trim(), " "))
             }
         }
-        this.operate(cachedPath, currentMethod, args)
         this.cachedPath = cachedPath
         toPath.addPath(cachedPath)
     }
 
-    private fun operate(finalPath: Path, method: String, args: List<SVGAPoint>) {
+    private fun operate(finalPath: Path, method: String, args: StringTokenizer) {
+        var x0 = 0.0f
+        var y0 = 0.0f
+        var x1 = 0.0f
+        var y1 = 0.0f
+        var x2 = 0.0f
+        var y2 = 0.0f
+        try {
+            var index = 0
+            while (args.hasMoreTokens()) {
+                val s = args.nextToken()
+                if (index == 0) { x0 = s.toFloat() }
+                if (index == 1) { y0 = s.toFloat() }
+                if (index == 2) { x1 = s.toFloat() }
+                if (index == 3) { y1 = s.toFloat() }
+                if (index == 4) { x2 = s.toFloat() }
+                if (index == 5) { y2 = s.toFloat() }
+                index++
+            }
+        } catch (e: Exception) {}
         var currentPoint = SVGAPoint(0.0f, 0.0f, 0.0f)
-        if (method == "M" && args.size == 1) {
-            finalPath.moveTo(args[0].x, args[0].y)
-            currentPoint = SVGAPoint(args[0].x, args[0].y, 0.0f)
-        } else if (method == "m" && args.size == 1) {
-            finalPath.rMoveTo(args[0].x, args[0].y)
-            currentPoint = SVGAPoint(currentPoint.x + args[0].x, currentPoint.y + args[0].y, 0.0f)
+        if (method == "M") {
+            finalPath.moveTo(x0, y0)
+            currentPoint = SVGAPoint(x0, y0, 0.0f)
+        } else if (method == "m") {
+            finalPath.rMoveTo(x0, y0)
+            currentPoint = SVGAPoint(currentPoint.x + x0, currentPoint.y + y0, 0.0f)
         }
-        if (method == "L" && args.size == 1) {
-            finalPath.lineTo(args[0].x, args[0].y)
-        } else if (method == "l" && args.size == 1) {
-            finalPath.rLineTo(args[0].x, args[0].y)
+        if (method == "L") {
+            finalPath.lineTo(x0, y0)
+        } else if (method == "l") {
+            finalPath.rLineTo(x0, y0)
         }
-        if (method == "C" && args.size == 3) {
-            finalPath.cubicTo(args[0].x, args[0].y, args[1].x, args[1].y, args[2].x, args[2].y)
-        } else if (method == "c" && args.size == 3) {
-            finalPath.rCubicTo(args[0].x, args[0].y, args[1].x, args[1].y, args[2].x, args[2].y)
+        if (method == "C") {
+            finalPath.cubicTo(x0, y0, x1, y1, x2, y2)
+        } else if (method == "c") {
+            finalPath.rCubicTo(x0, y0, x1, y1, x2, y2)
         }
-        if (method == "Q" && args.size == 2) {
-            finalPath.quadTo(args[0].x, args[0].y, args[1].x, args[1].y)
-        } else if (method == "q" && args.size == 2) {
-            finalPath.rQuadTo(args[0].x, args[0].y, args[1].x, args[1].y)
+        if (method == "Q") {
+            finalPath.quadTo(x0, y0, x1, y1)
+        } else if (method == "q") {
+            finalPath.rQuadTo(x0, y0, x1, y1)
         }
-        if (method == "H" && args.size == 1) {
-            finalPath.lineTo(args[0].value, currentPoint.y)
-        } else if (method == "h" && args.size == 1) {
-            finalPath.rLineTo(args[0].value, 0f)
+        if (method == "H") {
+            finalPath.lineTo(x0, currentPoint.y)
+        } else if (method == "h") {
+            finalPath.rLineTo(x0, 0f)
         }
-        if (method == "V" && args.size == 1) {
-            finalPath.lineTo(currentPoint.x, args[0].value)
-        } else if (method == "v" && args.size == 1) {
-            finalPath.rLineTo(0f, args[0].value)
+        if (method == "V") {
+            finalPath.lineTo(currentPoint.x, x0)
+        } else if (method == "v") {
+            finalPath.rLineTo(0f, x0)
         }
-        if (method == "Z" && args.size == 1) {
+        if (method == "Z") {
             finalPath.close()
-        } else if (method == "z" && args.size == 1) {
+        }
+        else if (method == "z") {
             finalPath.close()
         }
     }
