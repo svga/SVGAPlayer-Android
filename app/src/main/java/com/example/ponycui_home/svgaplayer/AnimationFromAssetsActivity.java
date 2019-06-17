c4d71e831c8001cf997e4bc25dc61beabb22ff44
errnull <errnull.top@gmail.com>
2019-06-17 15:53:18 +0800

    fix: correct demo.

--- app/src/main/java/com/example/ponycui_home/svgaplayer/AnimationFromAssetsActivity.java
+++ app/src/main/java/com/example/ponycui_home/svgaplayer/AnimationFromAssetsActivity.java
@@ -37,7 +37,7 @@ public class AnimationFromAssetsActivity extends Activity {
 
     private void loadAnimation() {
         SVGAParser parser = new SVGAParser(this);
-        parser.decodeFromAssets("matteRect.svga", new SVGAParser.ParseCompletion() {
+        parser.decodeFromAssets(this.randomSample(), new SVGAParser.ParseCompletion() {
             @Override
             public void onComplete(@NotNull SVGAVideoEntity videoItem) {
                 animationView.setVideoItem(videoItem);
