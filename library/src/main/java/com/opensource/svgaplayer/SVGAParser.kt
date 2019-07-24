e6d72ef14b28806fea31b3af4d89661d459e244f
PonyCui <cuiminghui2@bigo.sg>
2019-07-24 14:20:54 +0800

    fix: Use shared ThreadPoolExecutor avoid p_thread create OOM.

--- library/src/main/java/com/opensource/svgaplayer/SVGAParser.kt
+++ library/src/main/java/com/opensource/svgaplayer/SVGAParser.kt
@@ -40,7 +40,7 @@ class SVGAParser(private val context: Context) {
             val cancelBlock = {
                 cancelled = true
             }
-            Thread {
+            threadPoolExecutor.execute {
                 try {
                     if (HttpResponseCache.getInstalled() == null && !noCache) {
                         Log.e("SVGAParser", "SVGAParser can not handle cache before install HttpResponseCache. see https://github.com/yyued/SVGAPlayer-Android#cache")
@@ -65,7 +65,7 @@ class SVGAParser(private val context: Context) {
                                     outputStream.write(buffer, 0, count)
                                 }
                                 if (cancelled) {
-                                    return@Thread
+                                    return@execute
                                 }
                                 ByteArrayInputStream(outputStream.toByteArray()).use {
                                     complete(it)
@@ -77,7 +77,7 @@ class SVGAParser(private val context: Context) {
                     e.printStackTrace()
                     failure(e)
                 }
-            }.start()
+            }
             return cancelBlock
         }
 
@@ -87,7 +87,7 @@ class SVGAParser(private val context: Context) {
 
     companion object {
         private val threadPoolBlockingQueue = LinkedBlockingQueue<Runnable>()
-        private var threadPoolExecutor = ThreadPoolExecutor(3, 10, 60000, TimeUnit.MILLISECONDS, threadPoolBlockingQueue)
+        internal var threadPoolExecutor = ThreadPoolExecutor(3, 10, 60000, TimeUnit.MILLISECONDS, threadPoolBlockingQueue)
         fun setThreadPoolExecutor(executor: ThreadPoolExecutor) {
             threadPoolExecutor = executor
         }
