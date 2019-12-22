4e6363a4b089f77d9fecb6bb3ee2abf55b794a20
chenrenzhan <chenrenzhan@yy.com>
2019-12-22 23:01:54 +0800

    wire 反序列化

--- library/src/main/java/com/opensource/svgaplayer/SVGAImageView.kt
+++ library/src/main/java/com/opensource/svgaplayer/SVGAImageView.kt
@@ -4,7 +4,6 @@ import android.animation.Animator
 import android.animation.ValueAnimator
 import android.annotation.SuppressLint
 import android.content.Context
-import android.graphics.drawable.Drawable
 import android.os.Build
 import android.util.AttributeSet
 import android.util.Log
@@ -13,7 +12,6 @@ import android.view.View
 import android.view.animation.LinearInterpolator
 import android.widget.ImageView
 import com.opensource.svgaplayer.utils.SVGARange
-import java.lang.ref.WeakReference
 import java.net.URL
 
 /**
